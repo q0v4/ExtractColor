65f7f26d83c1b2828befd22d3db8b45058aa04aa
q0v4 <sigureyotuba@gmail.com>
2021-02-13 11:00:00 +0900

    CLI実装

--- main.py
+++ main.py
@@ -1,51 +1,64 @@
 import os
-from PIL import Image
+import sys
 from distutils.dir_util import copy_tree
 
-# todo: environmentふぁいるの個別設定
+import execute
 
 
 def main():
     path = "./result/textures"
     copy_tree("./textures", path)
 
+    print("[EC] >> Activate")
+    if not os.path.exists(path):
+        print("[EC] >> ファイル内に変換対象のファイルが入っていません。"
+              "[EC] >> 実行ファイルと同じディレクトリ内にtexturesファイルを入れた後、再度実行してください。\n")
+        sys.exit()
+
+    exe_mode = int(input("[EC] >> 処理内容を選んでください(グレースケール:0, 色抽出:1): "))
+    if not 0 <= exe_mode <= 1:
+        print("[EC] >> 入力値が不正です。指定された数値を入力してください")
+        sys.exit()
+
+    if exe_mode == 0:
+        print("[EC] >> 処理を開始します。(処理予想時間: 1~5分)")
+        for pathname, dir_names, filenames in os.walk(path):
+            for filename in filenames:
+                if filename.endswith(".png"):
+                    execute.grayscale(pathname + "/" + filename)
+
+        print("[EC] >> 処理が完了しました。")
+        sys.exit()
+
+    color_mode = int(input("[EC] >> 抜き出したい色を選択してください(赤:0, 緑:1, 青:2): "))
+    if not 0 <= color_mode <= 2:
+        print("[EC] >> 入力値が不正です。指定された数値を入力してください")
+        sys.exit()
+
+    color_range = (int(input("[EC] >> 抜き出す色の最小値を入力してください(0 ~ 255): ")),
+                   int(input("[EC] >> 抜き出す色の最大値を入力してください(0 ~ 255, 最小値より大きい): ")))
+    if color_range[0] > color_range[1]:
+        print("[EC] >> 最大値の数値が不正です。最大値より小さい値を入力してください")
+        sys.exit()
+
+    elif not 0 <= color_range[0] <= 255 or not 0 <= color_range[1] <= 255:
+        print("[EC] >> 入力値が不正です。範囲内の数値を指定してください")
+        sys.exit()
+
+    threshold = int(input("[EC] >> \"しきい値\"は抜き出したい色以外の色の差の許容値となります\n"
+                          "[EC] >> これが大きいほど抜き出したい色の近似色を拾いますが、想定していない色を拾う可能性が高くなります\n"
+                          "[EC] >> しきい値を入力してください(0 ~ 255): "))
+    if not 0 <= threshold <= 255:
+        print("[EC] >> 入力値が不正です。範囲内の数値を指定してください")
+        sys.exit()
+
+    print("[EC] >> 処理を開始します...(処理予想時間: 1~5分)")
     for pathname, dir_names, filenames in os.walk(path):
         for filename in filenames:
             if filename.endswith(".png"):
-                execute(pathname + "/" + filename, 15)
+                execute.extractColor(pathname + "/" + filename, color_mode, color_range, 15)
 
-
-def execute(file, threshold):
-    img = Image.open(file).convert("RGBA")
-    gray = Image.new("RGBA", img.size)
-    px = img.load()
-    counter = 0
-    red = [80, 255]
-
-    # ピクセルごとに条件合致するか調べ新しい画像として生成
-    for x in range(img.size[0]):
-        for y in range(img.size[1]):
-            pixel = px[x, y]
-            saturation = pixel[0]
-
-            if red[0] <= pixel[0] <= red[1] and abs(pixel[1] - pixel[2]) <= threshold and pixel[1] <= pixel[0] and pixel[2] <= pixel[0]:
-                gray.putpixel((x, y), pixel)
-                counter += 1
-
-            else:
-                gray.putpixel((x, y), (saturation, saturation, saturation, pixel[3]))
-
-    # if counter >= img.size[0] * img.size[1] / 5:
-    #     img.save(file)
-    #
-    # else:
-    #     for x in range(img.size[0]):
-    #         for y in range(img.size[1]):
-    #             pixel = px[x, y]
-    #             saturation = pixel[0]
-    #             gray.putpixel((x, y), (saturation, saturation, saturation, pixel[3]))
-
-    gray.save(file)
+    print("[EC] >> 処理が完了しました。")
 
 
 if __name__ == "__main__":
